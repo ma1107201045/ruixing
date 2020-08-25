acf8aa54ba32b873f5e9c0b5457f7741e1f8bf44
111 <111>
2020-08-25 20:35:07 +0800

    上传Excel

--- ruixing-entity/src/main/java/com/yintu/ruixing/guzhangzhenduan/XianDuanEntity.java
+++ ruixing-entity/src/main/java/com/yintu/ruixing/guzhangzhenduan/XianDuanEntity.java
@@ -37,6 +37,8 @@ public class XianDuanEntity {
 
     private Long xdType;
 
+    private String dwdName;
+
     private Integer xdState;//线段设置状态  0：未设置  1：已设置
 
     private String xdJson;//储存的json
