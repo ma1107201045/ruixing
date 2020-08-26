bec4e1713504f2c3e342daab32da1a0437882e68
111 <111>
2020-08-26 18:41:03 +0800

    上传Excel

--- ruixing-service/src/main/java/com/yintu/ruixing/paigongguanli/impl/PaiGongGuanLiPaiGongDanServiceImpl.java
+++ ruixing-service/src/main/java/com/yintu/ruixing/paigongguanli/impl/PaiGongGuanLiPaiGongDanServiceImpl.java
@@ -6,9 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.List;
+import java.net.InetAddress;
+import java.util.*;
 
 /**
  * @Author Mr.liu
@@ -86,16 +85,17 @@ public class PaiGongGuanLiPaiGongDanServiceImpl implements PaiGongGuanLiPaiGongD
                 }
             }
 
+            List lianxu = new ArrayList();
             //遍历符合条件的人员userlist
             if (userlist.size() != 0) {
-                double lianxutime = 0;
+                long lianxutime = 0;
                 for (int i = 0; i < userlist.size(); i++) {
                     String username = userlist.get(i);//得到人员名
                     List<PaiGongGuanLiPaiGongDanEntity> userList = paiGongGuanLiPaiGongDanDao.findUserByName(username);//根据人员名  查询此人的派工经历
                     if (userList.size() == 1) {
                         long endtime = userList.get(0).getChuchaiendtime().getTime();
                         long starttime = userList.get(0).getChuchaistarttime().getTime();
-                        lianxutime = endtime - starttime;
+                        lianxutime = (endtime - starttime) / 86400000;
                     }
                     if (userList.size() > 1) {
                         long endtime1 = userList.get(0).getChuchaiendtime().getTime();
@@ -107,19 +107,33 @@ public class PaiGongGuanLiPaiGongDanServiceImpl implements PaiGongGuanLiPaiGongD
                         long lianxutime1 = starttime1 - endtime2;
                         long dayNum = lianxutime1 / 86400000;//间隔天数
                         if (dayNum < 3) {
-                            lianxutime = (endtime1 - starttime1) + (endtime2 - starttime2);
+                            lianxutime = ((endtime1 - starttime1) + (endtime2 - starttime2)) / 86400000;
                         }
                         if (3 <= dayNum && dayNum < 6) {
-                            lianxutime = (endtime1 - starttime1) + (endtime2 - starttime2) * 0.6;
+                            lianxutime = (long) ((endtime1 - starttime1) + (endtime2 - starttime2) * 0.6) / 86400000;
                         }
                         if (6 <= dayNum && dayNum < 10) {
-                            lianxutime = (endtime1 - starttime1) + (endtime2 - starttime2) * 0.3;
+                            lianxutime = (long) ((endtime1 - starttime1) + (endtime2 - starttime2) * 0.3) / 86400000;
                         }
                         if (10 <= dayNum) {
-                            lianxutime = (endtime1 - starttime1);
+                            lianxutime = (endtime1 - starttime1) / 86400000;
                         }
                     }
+                    lianxu.add(lianxutime);
                 }
+
+                Object[] objects = lianxu.toArray();
+                double[] a = {};
+                for (int i = 0; i < lianxu.size(); i++) {
+                    lianxu.get(i);
+                }
+                //对连续时间排序
+                int n = lianxu.size();
+                HashMap map = new HashMap();
+                for (int i = 0; i < lianxu.size(); i++) {
+                    map.put(lianxu.get(i), i); //将值和下标存入Map
+                }
+
 
             }
         }
@@ -132,11 +146,12 @@ public class PaiGongGuanLiPaiGongDanServiceImpl implements PaiGongGuanLiPaiGongD
 
 
     public static void main(String[] args) {
-        int[] a={1,2,9,6,5};
+        /*int[] a={1,2,9,6,5};
+        int temp =0;
         for (int i = 0; i < a.length; i++) {
             for (int i1 = i+1; i1 < a.length; i1++) {
                 if (a[i]>a[i1]){
-                    int temp = a[i];
+                     temp = a[i];
 
                     a[i] = a[i1];
 
@@ -144,6 +159,95 @@ public class PaiGongGuanLiPaiGongDanServiceImpl implements PaiGongGuanLiPaiGongD
                 }
             }
         }
+        System.out.println(temp);*/
+
+        List list12 = new ArrayList();
+        int[] arr = {23, 12, 48, 56, 45};
+        int n1 = arr.length;
+
+        HashMap map1 = new HashMap();
+        for (int i = 0; i < arr.length; i++) {
+            map1.put(arr[i], i); //将值和下标存入Map
+        }
+
+
+        int temp = -1;
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[i] < arr[j]) {
+                    temp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = temp;
+
+                }
+            }
+        }
+        System.out.println(Arrays.toString(arr));
+        //查找原始下标
+
+        for (int i = 0; i < n1; i++) {
+            //System.out.println(map.get(a[i]));
+            list12.add("666" + map1.get(arr[i]));
+        }
+
+
+        ///////////////////////////////////
+
+        int n = 9;
+        int[] a = {8, 5, 4, 6, 2, 1, 7, 9, 3};
+        HashMap map = new HashMap();
+        for (int i = 0; i < a.length; i++) {
+            map.put(a[i], i); //将值和下标存入Map
+        }
+
+        //排列
+        List list = new ArrayList();
+        Arrays.sort(a); //升序排列
+        for (int i = 0; i < a.length; i++) {
+            list.add(a[i]);
+        }
+
+        Collections.reverse(list); //逆序排列,变为降序
+        for (int i = 0; i < list.size(); i++) {
+            a[i] = (Integer) list.get(i);
+        }
+
+        List list1 = new ArrayList();
+        //查找原始下标
+        for (int i = 0; i < n; i++) {
+            list1.add(map.get(a[i]));
+
+        }
+        System.out.println(list1);
+
+
+
+        //获取IP地址
+
+        try {
+            InetAddress myip= InetAddress.getLocalHost();
+
+            System.out.println("你的IP地址是："+myip.getHostAddress());
+
+            System.out.println("主机名为："+myip.getHostName()+"。");
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     }
 
 }
