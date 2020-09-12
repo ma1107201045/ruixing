73d735fba327b7d9c765aa241035b3df7ca94f53
global_mwy <global_1107201045@qq.com>
2020-09-12 13:51:04 +0800

    Edit too much files

--- ruixing-controller/src/main/java/com/yintu/ruixing/yunxingweihu/SparePartsController.java
+++ ruixing-controller/src/main/java/com/yintu/ruixing/yunxingweihu/SparePartsController.java
@@ -6,11 +6,11 @@ import com.github.pagehelper.PageInfo;
 import com.yintu.ruixing.common.SessionController;
 import com.yintu.ruixing.common.util.BaseController;
 import com.yintu.ruixing.common.util.ResponseDataUtil;
-import com.yintu.ruixing.guzhangzhenduan.CheZhanEntity;
-import com.yintu.ruixing.guzhangzhenduan.CheZhanService;
+import com.yintu.ruixing.guzhangzhenduan.*;
 import com.yintu.ruixing.weixiudaxiu.EquipmentEntity;
 import com.yintu.ruixing.weixiudaxiu.EquipmentService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
@@ -18,6 +18,7 @@ import org.springframework.web.multipart.MultipartFile;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
+import java.util.Date;
 import java.util.List;
 import java.util.Map;
 
@@ -27,71 +28,94 @@ import java.util.Map;
  * @author:mlf
  * @date:2020/7/11 14:23
  */
-@RestController
+@Controller
 @RequestMapping("/spare/parts")
 public class SparePartsController extends SessionController implements BaseController<SparePartsEntity, Integer> {
 
     @Autowired
     private SparePartsService sparePartsService;
+
     @Autowired
-    private CheZhanService cheZhanService;
+    private SparePartsInfoService sparePartsInfoService;
+
     @Autowired
-    private EquipmentService equipmentService;
+    private DataStatsService dataStatsService;
 
     @PostMapping
+    @ResponseBody
     public Map<String, Object> add(@Validated SparePartsEntity entity) {
+        entity.setCreateBy(this.getLoginUserName());
+        entity.setCreateTime(new Date());
+        entity.setModifiedBy(this.getLoginUserName());
+        entity.setModifiedTime(new Date());
+        entity.setIsStart((short) 1);
         sparePartsService.add(entity);
-        return ResponseDataUtil.ok("添加备品实验信息成功");
+        return ResponseDataUtil.ok("添加备品试验信息成功");
     }
 
     @Override
     public Map<String, Object> remove(Integer id) {
-
         return null;
     }
 
+
     @DeleteMapping("/{ids}")
+    @ResponseBody
     public Map<String, Object> remove(@PathVariable Integer[] ids) {
         sparePartsService.remove(ids);
-        return ResponseDataUtil.ok("删除备品实验信息成功");
+        return ResponseDataUtil.ok("删除备品试验信息成功");
     }
 
-
     @PutMapping("/{id}")
+    @ResponseBody
     public Map<String, Object> edit(@PathVariable Integer id, @Validated SparePartsEntity entity) {
+        entity.setModifiedBy(this.getLoginUserName());
+        entity.setModifiedTime(new Date());
         sparePartsService.edit(entity);
-        return ResponseDataUtil.ok("更新备品实验信息成功");
+        return ResponseDataUtil.ok("修改备品试验信息成功");
     }
 
     @GetMapping("/{id}")
+    @ResponseBody
     public Map<String, Object> findById(@PathVariable Integer id) {
         SparePartsEntity sparePartsEntity = sparePartsService.findById(id);
-        return ResponseDataUtil.ok("查询备品实验信息成功", sparePartsEntity);
+        return ResponseDataUtil.ok("查询备品试验信息成功", sparePartsEntity);
     }
 
+
     @GetMapping
-    public Map<String, Object> findAll(@RequestParam("page_number") Integer pageNumber,
-                                       @RequestParam("page_size") Integer pageSize,
-                                       @RequestParam(value = "order_by", required = false, defaultValue = "sp.id DESC") String orderBy,
-                                       @RequestParam(value = "equipment_name", required = false) String equipmentName) {
+    @ResponseBody
+    public Map<String, Object> findByAll(@RequestParam("page_number") Integer pageNumber,
+                                         @RequestParam("page_size") Integer pageSize,
+                                         @RequestParam(value = "order_by", required = false, defaultValue = "mp.id DESC") String orderBy,
+                                         @RequestParam(value = "name", required = false) String name) {
         PageHelper.startPage(pageNumber, pageSize, orderBy);
-        List<SparePartsEntity> sparePartsEntities = sparePartsService.findByExample(null, equipmentName);
+        List<SparePartsEntity> sparePartsEntities = sparePartsService.findByExample(null, name);
         PageInfo<SparePartsEntity> pageInfo = new PageInfo<>(sparePartsEntities);
-        return ResponseDataUtil.ok("查询备品实验列表信息成功", pageInfo);
+        return ResponseDataUtil.ok("查询备品试验信息列表成功", pageInfo);
     }
+
 
     @PostMapping("/import")
     @ResponseBody
     public Map<String, Object> importFile(@RequestParam("file") MultipartFile multipartFile) throws IOException {
-        sparePartsService.importFile(multipartFile.getInputStream(), multipartFile.getOriginalFilename());
-        return ResponseDataUtil.ok("导入备品实验信息成功");
+        String[][] context = sparePartsService.importFile(multipartFile.getInputStream(), multipartFile.getOriginalFilename());
+        return ResponseDataUtil.ok("获取导入备品试验信息成功", context);
     }
+
+    @PostMapping("/import/data")
+    @ResponseBody
+    public Map<String, Object> importDate(@RequestBody String[][] context) {
+        sparePartsService.importDate(context, this.getLoginUserName());
+        return ResponseDataUtil.ok("导入备品试验信息成功");
+    }
+
 
     @GetMapping("/template")
     public void templateFile(HttpServletResponse response) throws IOException {
-        String fileName = "备品实验列表-模板" + DateUtil.now() + ".xlsx";
-        response.setContentType("application/octet-stream;charset=ISO-8859-1");
-        response.setHeader("Content-Disposition", "attachment;filename=" + new String(fileName.getBytes(), StandardCharsets.ISO_8859_1));
+        String fileName = "备品试验列表-模板" + DateUtil.now() + ".xlsx";
+        response.setContentType("application/octet-stream;charset=ISO8859-1");
+        response.setHeader("Content-Disposition", "attachment;filename=" + new String(fileName.getBytes(), "ISO8859-1"));
         response.addHeader("Pargam", "no-cache");
         response.addHeader("Cache-Control", "no-cache");
         sparePartsService.templateFile(response.getOutputStream());
@@ -99,36 +123,54 @@ public class SparePartsController extends SessionController implements BaseContr
 
     @GetMapping("/export/{ids}")
     public void exportFile(@PathVariable Integer[] ids, HttpServletResponse response) throws IOException {
-        String fileName = "备品实验列表-导出" + DateUtil.now() + ".xlsx";
-        response.setContentType("application/octet-stream;charset=ISO-8859-1");
-        response.setHeader("Content-Disposition", "attachment;filename=" + new String(fileName.getBytes(), StandardCharsets.ISO_8859_1));
+        String fileName = "备品试验列表-导出" + DateUtil.now() + ".xlsx";
+        response.setContentType("application/octet-stream;charset=ISO8859-1");
+        response.setHeader("Content-Disposition", "attachment;filename=" + new String(fileName.getBytes(), "ISO8859-1"));
         response.addHeader("Pargam", "no-cache");
         response.addHeader("Cache-Control", "no-cache");
         sparePartsService.exportFile(response.getOutputStream(), ids);
     }
 
-    /**
-     * 查询全部车站信息
-     *
-     * @return
-     */
-    @GetMapping("/chezhans")
+
+    @GetMapping("/{id}/spare/parts/infos")
     @ResponseBody
-    public Map<String, Object> findCheZhanAll() {
-        List<CheZhanEntity> cheZhanEntities = cheZhanService.findAll();
-        return ResponseDataUtil.ok("查询车站信息列表成功", cheZhanEntities);
+    public Map<String, Object> findMaintenancePlanInfoById(@PathVariable Integer id, @RequestParam("page_number") Integer pageNumber,
+                                                           @RequestParam("page_size") Integer pageSize,
+                                                           @RequestParam(value = "order_by", required = false, defaultValue = "spi.id DESC") String orderBy) {
+        PageHelper.startPage(pageNumber, pageSize, orderBy);
+        List<SparePartsInfoEntity> sparePartsInfoEntities = sparePartsInfoService.findByCondition(null, id, null);
+        PageInfo<SparePartsInfoEntity> pageInfo = new PageInfo<>(sparePartsInfoEntities);
+        return ResponseDataUtil.ok("查询备品试验记录列表信息成功", pageInfo);
     }
 
-
-    /**
-     * 查询设备全部信息
-     *
-     * @return
-     */
-    @GetMapping("/equipments")
+    @GetMapping("/railways/bureau")
     @ResponseBody
-    public Map<String, Object> findEquipmentAll() {
-        List<EquipmentEntity> equipmentEntities = equipmentService.findAll();
-        return ResponseDataUtil.ok("查询设备信息列表成功", equipmentEntities);
+    public Map<String, Object> findAllTieLuJu() {
+        List<TieLuJuEntity> tieLuJuEntities = dataStatsService.findAllTieLuJu();
+        return ResponseDataUtil.ok("查询铁路局成功", tieLuJuEntities);
+    }
+
+    //根据铁路局的id  查询对应的电务段
+    @GetMapping("/signal/depot")
+    @ResponseBody
+    public Map<String, Object> findDianWuDuanByTid(@RequestParam Integer railwaysBureauId) {
+        List<DianWuDuanEntity> dianWuDuanEntities = dataStatsService.findDianWuDuanByTid(railwaysBureauId);
+        return ResponseDataUtil.ok("查询电务段成功", dianWuDuanEntities);
+    }
+
+    //根据电务段的id  查找线段的名字和id
+    @GetMapping("/special/railway/line")
+    @ResponseBody
+    public Map<String, Object> findXianDuanByDid(@RequestParam Integer signalDepotId) {
+        List<XianDuanEntity> xianDuanEntities = dataStatsService.findXianDuanByDid(signalDepotId);
+        return ResponseDataUtil.ok("查询线段成功", xianDuanEntities);
+    }
+
+    //根据线段的id  查找车站的名字和id
+    @GetMapping("/station")
+    @ResponseBody
+    public Map<String, Object> findCheZhanByXid(@RequestParam Integer specialRailwayLineId) {
+        List<CheZhanEntity> cheZhanEntities = dataStatsService.findCheZhanByXid(specialRailwayLineId);
+        return ResponseDataUtil.ok("查询车站成功", cheZhanEntities);
     }
 }
