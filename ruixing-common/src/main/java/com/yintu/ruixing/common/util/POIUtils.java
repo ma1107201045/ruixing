1a266a1776fcdaa966463ee75774917077cc2f14
global_mwy <global_1107201045@qq.com>
2020-10-31 14:52:40 +0800

    Edit too much files

--- ruixing-common/src/main/java/com/yintu/ruixing/common/util/POIUtils.java
+++ ruixing-common/src/main/java/com/yintu/ruixing/common/util/POIUtils.java
@@ -19,8 +19,10 @@ public class POIUtils {
     private final static String xls = "xls";
     private final static String xlsx = "xlsx";
     private final static String DATE_FORMAT = "yyyy/MM/dd";
+
     /**
      * 读入excel文件，解析后返回
+     *
      * @param file
      * @throws IOException
      */
@@ -31,34 +33,36 @@ public class POIUtils {
         Workbook workbook = getWorkBook(file);
         //创建返回对象，把每行中的值作为一个数组，所有行作为一个集合返回
         List<String[]> list = new ArrayList<String[]>();
-        if(workbook != null){
-            for(int sheetNum = 0;sheetNum < workbook.getNumberOfSheets();sheetNum++){
+        if (workbook != null) {
+            for (int sheetNum = 0; sheetNum < workbook.getNumberOfSheets(); sheetNum++) {
                 //获得当前sheet工作表
                 Sheet sheet = workbook.getSheetAt(sheetNum);
-                System.out.println("1211212"+sheet.getSheetName());
-                if(sheet == null){
+                System.out.println("1211212" + sheet.getSheetName());
+                if (sheet == null) {
                     continue;
                 }
                 //获得当前sheet的开始行
-                int firstRowNum  = sheet.getFirstRowNum();
-                System.out.println("firsttttttttttttttt"+firstRowNum);
+                int firstRowNum = sheet.getFirstRowNum();
+                System.out.println("firsttttttttttttttt" + firstRowNum);
                 //获得当前sheet的结束行
                 int lastRowNum = sheet.getLastRowNum();
-                System.out.println("lasttttttttttttttttttttttttt"+lastRowNum);
+                System.out.println("lasttttttttttttttttttttttttt" + lastRowNum);
                 //循环除了第二行的所有行
-                for(int rowNum = firstRowNum+2;rowNum <= lastRowNum;rowNum++){
+                for (int rowNum = firstRowNum + 2; rowNum <= lastRowNum; rowNum++) {
                     //获得当前行
                     Row row = sheet.getRow(rowNum);
-                    if(row == null){
+                    if (row == null) {
                         continue;
                     }
                     //获得当前行的开始列
                     int firstCellNum = row.getFirstCellNum();
                     //获得当前行的列数
-                    int lastCellNum = row.getPhysicalNumberOfCells();
+
+                    //int lastCellNum = row.getPhysicalNumberOfCells();
+                    int lastCellNum = row.getLastCellNum();
                     String[] cells = new String[row.getPhysicalNumberOfCells()];
                     //循环当前行
-                    for(int cellNum = firstCellNum; cellNum < lastCellNum;cellNum++){
+                    for (int cellNum = firstCellNum; cellNum < lastCellNum; cellNum++) {
                         Cell cell = row.getCell(cellNum);
                         cells[cellNum] = getCellValue(cell);
                     }
@@ -71,18 +75,19 @@ public class POIUtils {
     }
 
     //校验文件是否合法
-    public static void checkFile(MultipartFile file) throws IOException{
+    public static void checkFile(MultipartFile file) throws IOException {
         //判断文件是否存在
-        if(null == file){
+        if (null == file) {
             throw new FileNotFoundException("文件不存在！");
         }
         //获得文件名
         String fileName = file.getOriginalFilename();
         //判断文件是否是excel文件
-        if(!fileName.endsWith(xls) && !fileName.endsWith(xlsx)){
+        if (!fileName.endsWith(xls) && !fileName.endsWith(xlsx)) {
             throw new IOException(fileName + "不是excel文件");
         }
     }
+
     public static Workbook getWorkBook(MultipartFile file) {
         //获得文件名
         String fileName = file.getOriginalFilename();
@@ -92,10 +97,10 @@ public class POIUtils {
             //获取excel文件的io流
             InputStream is = file.getInputStream();
             //根据文件后缀名不同(xls和xlsx)获得不同的Workbook实现类对象
-            if(fileName.endsWith(xls)){
+            if (fileName.endsWith(xls)) {
                 //2003
                 workbook = new HSSFWorkbook(is);
-            }else if(fileName.endsWith(xlsx)){
+            } else if (fileName.endsWith(xlsx)) {
                 //2007
                 workbook = new XSSFWorkbook(is);
             }
@@ -104,23 +109,24 @@ public class POIUtils {
         }
         return workbook;
     }
-    public static String getCellValue(Cell cell){
+
+    public static String getCellValue(Cell cell) {
         String cellValue = "";
-        if(cell == null){
+        if (cell == null) {
             return cellValue;
         }
         //如果当前单元格内容为日期类型，需要特殊处理
         String dataFormatString = cell.getCellStyle().getDataFormatString();
-        if(dataFormatString.equals("m/d/yy")){
+        if (dataFormatString.equals("m/d/yy")) {
             cellValue = new SimpleDateFormat(DATE_FORMAT).format(cell.getDateCellValue());
             return cellValue;
         }
         //把数字当成String来读，避免出现1读成1.0的情况
-        if(cell.getCellType() == Cell.CELL_TYPE_NUMERIC){
+        if (cell.getCellType() == Cell.CELL_TYPE_NUMERIC) {
             cell.setCellType(Cell.CELL_TYPE_STRING);
         }
         //判断数据的类型
-        switch (cell.getCellType()){
+        switch (cell.getCellType()) {
             case Cell.CELL_TYPE_NUMERIC: //数字
                 cellValue = String.valueOf(cell.getNumericCellValue());
                 break;
