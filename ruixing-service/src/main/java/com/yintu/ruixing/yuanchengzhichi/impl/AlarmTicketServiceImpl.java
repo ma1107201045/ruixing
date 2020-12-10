fd302706cc2ae7cc01160ed00f35cf68da906b83
global_mwy <global_1107201045@qq.com>
2020-12-10 18:06:28 +0800

    edit AlarmDao.xml      AlarmTicketServiceImpl.java files

---
+++ ruixing-service/src/main/java/com/yintu/ruixing/yuanchengzhichi/impl/AlarmTicketServiceImpl.java
@@ -0,0 +1,16 @@
+package com.yintu.ruixing.yuanchengzhichi.impl;
+
+import com.yintu.ruixing.yuanchengzhichi.AlarmTicketService;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+/**
+ * @Author: mlf
+ * @Date: 2020/12/10 17:27
+ * @Version: 1.0
+ */
+@Service
+@Transactional
+public class AlarmTicketServiceImpl implements AlarmTicketService {
+
+}
