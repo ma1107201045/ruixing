b5f8bfad32e79e2dddc3a4fe280a948f3a6deaea
global_mwy <global_1107201045@qq.com>
2020-12-19 21:36:40 +0800

    edit AuditTotalEntity.java files

--- ruixing-entity/src/main/java/com/yintu/ruixing/common/AuditTotalEntity.java
+++ ruixing-entity/src/main/java/com/yintu/ruixing/common/AuditTotalEntity.java
@@ -1,9 +1,17 @@
 package com.yintu.ruixing.common;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import java.io.Serializable;
 import java.util.Date;
 
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class AuditTotalEntity implements Serializable {
+    private static final long serialVersionUID = 6029448212998712680L;
     private Integer id;
 
     private String title;
